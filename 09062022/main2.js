b6c55e74e795e972be80dc714348a6b78940001c
Lazbonita <kipronocaleb@yahoo.com>
2022-06-09 23:03:26 +0300

    reverse number to an array

---
+++ 09062022/main2.js
@@ -0,0 +1,11 @@
+// Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+// Example(Input => Output):
+
+
+function digitize(n) {
+    return String(n).split('').map(Number).reverse()
+  }
+
+
