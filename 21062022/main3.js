4e012e822fa76e60cf0d46114c226128b189e444
Lazbonita <kipronocaleb@yahoo.com>
2022-06-21 23:02:29 +0300

    body and tail

---
+++ 21062022/main3.js
@@ -0,0 +1,9 @@
+// Create a function to make sure that the second argument (tail), 
+//is the same as the last letter of the first argument (body) 
+//- otherwise the tail wouldn't fit!
+
+function correctTail(bod, tail){
+    return bod[bod.length-1]==tail
+  }
+
+
